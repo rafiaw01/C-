5130d78cb801f07358e8eddbf1a2923670c04ea3
rafiaw01 <127777886+rafiaw01@users.noreply.github.com>
2023-05-01 20:15:34 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    int num1;
+    int num2;
+
+
+    cout << " enter the value of num1";
+    cin >> num1;
+
+     cout << " enter the value of num2";
+    cin >> num2;
+
+    int sum;
+    sum = num1 + num2 ;
+
+    cout<< " The sum of num1 and num2 is "<<sum<<endl;
+
+
+
+    return 0;
+}
